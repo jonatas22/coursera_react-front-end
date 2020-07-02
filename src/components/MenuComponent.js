25c716c4290a39e3a68c3ffbe85241a18cf8c386
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-07-01 23:30:58 -0300

    Initial Commit

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+
+const Menu = (props) => {
+	const menu = props.dishes.map((dish) => {
+		return (
+			<div key={dish.id} className="col-12 col-md-5 m-1">
+				{/* <Card onClick={() => props.onClick(dish.id)}> */}
+				<Card>
+					<CardImg width="100%" src={dish.image} alt={dish.name} />
+					<CardImgOverlay>
+						<CardTitle>{dish.name}</CardTitle>
+					</CardImgOverlay>
+				</Card>
+			</div>
+		);
+	});
+
+	return (
+		<div className="container"> 
+			<div className="row">
+				{menu}
+			</div>
+		</div>
+	);
+}
+
+export default Menu;
