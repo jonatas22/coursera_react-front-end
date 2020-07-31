9f7e9859e00f9c7ec8ebf1ea81ec9f51bfdb60b2
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-07-30 22:09:11 -0300

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,9 @@ import {Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem
         Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -29,7 +32,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 
         handleSubmit(values) {
             this.toggleCommentModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }        
 
         render() {
@@ -92,7 +95,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                                         }}
                                      />                                        
                                 </div>
-                                <Button type="submit" color="bg-primary">Submit</Button>
+                                <Button type="submit" color="primary">Submit</Button>
                             </LocalForm>
                         </ModalBody>
                     </Modal>
@@ -106,7 +109,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
             return (
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
@@ -120,7 +123,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
         }
     }
     
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if (comments != null) {
             return (
                     <React.Fragment>
@@ -134,7 +137,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                                         </ul>
                                     )
                                 })}
-                            <CommentForm dishId={dishId} addComment={addComment}/>
+                            <CommentForm dishId={dishId} postComment={postComment}/>
                         </div>                        
                     </React.Fragment>
             );
@@ -147,25 +150,46 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
     }
 
     const DishDetail = (props) => {
-        return (
-            <div className='container'>
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem ative>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
                     </div>
                 </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments}
-                                    addComment={props.addComment}
-                                    dishId={props.dish.id} />
-                </div>    
-            </div>);
+            )
+        }
+        else if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            )            
+        }        
+        else if (props.dish != null) {
+            return (
+                <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem ative>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments}
+                                        postComment={props.postComment}
+                                        dishId={props.dish.id} />
+                    </div>    
+                </div>
+            );
+        }
     }
         
 export default DishDetail;
